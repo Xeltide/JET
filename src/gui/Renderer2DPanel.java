2c1306f62bb3b6d38b351d2b5c77be8abfc8dfb0
Joshua Abe <joshua.t.abe@gmail.com>
2016-10-13 00:27:29 -0700

    Completed loading from destination source for Renderer 2D and saving

--- src/gui/Renderer2DPanel.java
+++ src/gui/Renderer2DPanel.java
@@ -1,21 +1,57 @@
 package gui;
 
+import java.awt.event.FocusAdapter;
+import java.awt.event.FocusEvent;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+
+import javax.swing.JLabel;
+import javax.swing.JPanel;
 import javax.swing.JTextField;
 
+import net.miginfocom.swing.MigLayout;
 import objects.BlockType;
 import objects.Renderer2D;
 import objects.VObject;
 
 public class Renderer2DPanel extends BlockPanel {
     
+    private VObject obj;
     private Renderer2D rend;
+    
+    private JPanel panel;
     private JTextField imgLink;
     
     public Renderer2DPanel(VObject obj) {
         super(BlockType.RENDERER_2D);
-        
-        imgLink = new JTextField();
-        add(imgLink);
+        panel = new JPanel();
+        panel.setLayout(new MigLayout("insets 0, wrap 4"));
+        this.obj = obj;
+        rend = (Renderer2D) obj.getBlockByType(BlockType.RENDERER_2D);
+        imgLink = new JTextField(200);
+        imgLink.addKeyListener(new KeyAdapter() {
+            
+            @Override
+            public void keyReleased(KeyEvent e) {
+                if (e.getKeyCode() == KeyEvent.VK_ENTER) {
+                    requestFocusInWindow();
+                    revalidate();
+                }
+            }
+        });
+        imgLink.addFocusListener(new FocusAdapter() {
+            
+            @Override
+            public void focusLost(FocusEvent e) {
+                rend.setImg(imgLink.getText());
+                obj.setBlockByType(BlockType.RENDERER_2D, rend);
+                JetMenu.main.objH.setVObject(obj.getName(), obj);
+            }
+        });
+        imgLink.setText(rend.getImg());
+        panel.add(new JLabel("File:"));
+        panel.add(imgLink);
+        add(panel, "newline");
     }
 
 }
--- src/objects/Renderer2D.java
+++ src/objects/Renderer2D.java
@@ -13,18 +13,26 @@ import gui.Renderer2DPanel;
 public class Renderer2D extends Block {
 
     private BufferedImage img;
+    private String link;
 
     Renderer2D() {
         super(BlockType.RENDERER_2D);
+        link = "C:/";
         img = null;
     }
 
-    void setImg(String location) {
+    public void setImg(String location) {
+        link = location;
         try {
-            img = ImageIO.read(new File(location));
+            img = ImageIO.read(new File(link));
         } catch (IOException e) {
+            img = null;
             Console.log("Error loading image.");
         }
+    }
+    
+    public String getImg() {
+        return link;
     }
 
     @Override
